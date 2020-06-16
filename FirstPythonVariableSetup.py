4202cd9c8b012942799c49e4a31671c20687eeda
nware49 <65460756+nware49@users.noreply.github.com>
2020-06-16 12:07:54 -0400

    Add files via upload

---
+++ FirstPythonVariableSetup.py
@@ -0,0 +1,18 @@
+#Data Compilation Setup
+time = 0
+int(time)
+aTime = (time + 10)
+bTime = (time + 5)
+cTime = (time + 20)
+a = 100
+b = 25
+c = 400
+sensorAlphaValue = a
+sensorBetaValue = b
+sensorCharlieValue = c
+aValue = (sensorAlphaValue * int(aTime))
+bValue = (sensorBetaValue * int(bTime))
+cValue = (sensorCharlieValue * int(cTime))
+print(aValue)
+print(bValue)
+print(cValue)

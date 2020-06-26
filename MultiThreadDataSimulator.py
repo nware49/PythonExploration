54dd12455b73eb5c41ac046f8233b935f861e34b
nware49 <65460756+nware49@users.noreply.github.com>
2020-06-26 14:22:47 -0400

    Add files via upload

---
+++ MultiThreadDataSimulator.py
@@ -0,0 +1,91 @@
+import serial
+import math
+import time
+import threading
+import random
+from datetime import datetime
+
+Port1 = "COM3" #This is which port the data will be sent from
+Port2 = "COM4"
+#Attempts to open and assign a serial port
+#If it cannot open the port, it will print an error message
+try:
+    ser1 = serial.Serial(Port1)
+    ser2 = serial.Serial(Port2)
+except:
+    print("Could not open ports. Port is either in use or does not exist.")
+    exit()
+print(ser1.name)
+print(ser2.name)
+
+class Serial1Write(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
+        super(Serial1Write,self).__init__()
+
+    def run(self):
+        a = -1.1
+        i = -1
+        pi = math.pi
+        while True:
+            i += 1
+            pi = math.pi
+            curDT = datetime.now()
+            Time = curDT.strftime("%H:%M:%S")
+            x = random.uniform(0,10)
+            RandFloat = format(x, '.5f')
+            y = math.sin((i*pi)/12)
+            SineWave = format(y, '.4f')
+            z = math.cos((i*pi)/12)
+            CosineWave = format(z, '.4f')
+            LongString = (str(i) + "," + str(Time) + "," + str(RandFloat) + "," + str(SineWave) + "," + str(CosineWave) + "\n")
+            try:
+                ser1.write(LongString.encode())
+                print (LongString)
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print("Cannot write to port.")
+                break
+        
+class Serial2Write(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None, verbose=None):
+        super(Serial2Write,self).__init__()
+
+    def run(self):
+        a = -1.1
+        i = -1
+        pi = math.pi
+        while True:
+            i += 1
+            a += 1.1
+            aFormat = format(a, '.1f')
+            curDT = datetime.now()
+            Time = curDT.strftime("%H:%M:%S")
+            x = random.uniform(0,10)
+            RandFloat = format(x, '.5f')
+            y = math.sin((i*pi)/12)
+            SineWave = format(y, '.4f')
+            z = math.cos((i*pi)/12)
+            CosineWave = format(z, '.4f')
+            LongString = (str(aFormat) + "," + str(Time) + "," + str(RandFloat) + "," + str(SineWave) + "," + str(CosineWave) + "\n")
+            try:
+                ser2.write(LongString.encode())
+                print (LongString)
+                time.sleep(1.1)
+            except KeyboardInterrupt:
+                print("Cannot write to port.")
+                break
+try:
+    if __name__ == '__main__':
+        writer1 = Serial1Write()
+        writer2 = Serial2Write()
+
+        writer1.start()
+        writer2.start()
+            
+except KeyboardInterrupt:
+    __name__ = '__quit__'
+    print("Keyboard Interrupt")
+    quit()
+except:
+    print("Cannot write to port.")
+    quit()
